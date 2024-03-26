0430d38976507f143959b82b5fdcbbe3166fd984
viswanand6187 <viswanandofficial@gmail.com>
2024-03-26 09:45:03 +0530

    first commit

---
+++ frontend/src/Pages/AboutUs.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero from "../Components/Hero";
+import Biography from "../Components/Biography";
+
+const AboutUs = () => {
+  return (
+    <>
+      <Hero
+        title={"Learn More About Us | VCare Medical Institute"}
+        imageUrl={"/about.png"}
+      />
+      <Biography imageUrl={"/whoweare.png"} />
+    </>
+  )
+}
+
+export default AboutUs
